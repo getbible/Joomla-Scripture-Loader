3c1695a7f95badc242c2e91b005070e205b5a53d
Llewellyn van der Merwe <llewellyn@vdm.io>
2023-11-09 08:16:35 +0200

    first commit

---
+++ getbibleloader.php
@@ -0,0 +1,56 @@
+<?php
+/*----------------------------------------------------------------------------------|  io.vdm.dev  |----/
+			Vast Development Method
+/-------------------------------------------------------------------------------------------------------/
+
+    @package    getBible.net
+
+    @created    3rd December, 2015
+    @author     Llewellyn van der Merwe <https://getbible.net>
+    @git        Get Bible <https://git.vdm.dev/getBible>
+    @github     Get Bible <https://github.com/getBible>
+    @support    Get Bible <https://git.vdm.dev/getBible/support>
+    @copyright  Copyright (C) 2015. All Rights Reserved
+    @license    GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html
+
+/------------------------------------------------------------------------------------------------------*/
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Html\HTMLHelper;
+
+/**
+ * System - GetBibleLoader plugin.
+ *
+ * @package   GetBibleLoader
+ * @since     2.0.0
+ */
+class PlgSystemGetBibleLoader extends CMSPlugin
+{
+	/**
+	 * Application object
+	 *
+	 * @var    CMSApplication
+	 * @since  1.0.0
+	 */
+	protected  $app;
+
+	/**
+	 * This method is called before the head is compiled and allows manipulation of the head data.
+	 * In this case, it's used to add a JavaScript file to the front end of the Joomla site.
+	 *
+	 * @return  void
+	 *
+	 * @since   3.0.0
+	 * @throws  Exception on error.
+	 */
+	public function onBeforeCompileHead()
+	{
+		// Check if we are in the site application
+		if ($this->app->isClient('site')) {
+			HTMLHelper::_('script', 'https://cdn.jsdelivr.net/gh/getbible/loader@2.0.0/dist/js/getBible.min.js', ['version' => 'auto']);
+		}
+	}
+}
---
+++ script.php
@@ -0,0 +1,64 @@
+<?php
+/*----------------------------------------------------------------------------------|  io.vdm.dev  |----/
+			Vast Development Method
+/-------------------------------------------------------------------------------------------------------/
+
+    @package    getBible.net
+
+    @created    3rd December, 2015
+    @author     Llewellyn van der Merwe <https://getbible.net>
+    @git        Get Bible <https://git.vdm.dev/getBible>
+    @github     Get Bible <https://github.com/getBible>
+    @support    Get Bible <https://git.vdm.dev/getBible/support>
+    @copyright  Copyright (C) 2015. All Rights Reserved
+    @license    GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html
+
+/------------------------------------------------------------------------------------------------------*/
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * System - GetBibleLoader script file.
+ *
+ * @package PlgSystemGetBibleLoader
+ */
+class plgSystemGetBibleLoaderInstallerScript
+{
+
+	/**
+	 * Called before any type of action
+	 *
+	 * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
+	 * @param   Joomla\CMS\Installer\InstallerAdapter  $adapter  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function preflight($route, $adapter)
+	{
+		// get application
+		$app = JFactory::getApplication();
+
+		// the default for both install and update
+		$jversion = new JVersion();
+		if (!$jversion->isCompatible('3.8.0'))
+		{
+			$app->enqueueMessage('Please upgrade to at least Joomla! 3.8.0 before continuing!', 'error');
+			return false;
+		}
+
+		if ('install' === $route)
+		{
+//
+
+			$jversion = new JVersion();
+			if (!$jversion->isCompatible('3.10.0')) {
+				$app->enqueueMessage('Please upgrade to at least Joomla! 3.10 before continuing!', 'error');
+				return false;
+			}
+
+		}
+
+		return true;
+	}
+}
